f43111bd10d57682c968dfe55ff146cb0ff57271
tolgakb <tolgakabaahmet@gmail.com>
2023-05-15 23:12:32 +0100

    Site scraper project has been completed

---
+++ myapp/views.py
@@ -0,0 +1,37 @@
+from urllib import request
+from django.shortcuts import redirect, render
+import requests
+from bs4 import BeautifulSoup
+from .models import Link
+from django.http import HttpResponseRedirect
+# Create your views here.
+
+def scrape(request):
+
+    if request.method == 'POST':
+        site = request.POST.get('site','')
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+
+        for link in soup.find_all('a'):
+            link_address = link.get('href')
+            link_text = link.string
+            Link.objects.create(address= link_address, name = link_text) 
+
+        return redirect('/')
+
+    else:
+
+        data = Link.objects.all()
+
+
+        context = {
+            'data': data,
+        }
+
+    return render(request, 'myapp/result.html', context)
+
+def clear(request):
+    Link.objects.all().delete()
+    return render(request, 'myapp/result.html')
